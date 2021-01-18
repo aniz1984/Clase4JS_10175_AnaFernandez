3441ebeea4735a5b003e07eb13471a575e642617
Ana Fernandez <ana.lfb@gmail.com>
2021-01-17 23:22:50 -0300

    Clase 4 Desafio

---
+++ script2.js
@@ -0,0 +1,37 @@
+var imc = 0;
+    
+function mostrarImc(){
+    var resultado = calcular();
+        
+    document.getElementById("calculoImc").innerHTML = `
+    <b>${resultado}</b>
+    `
+    
+    if (imc <= 18){
+        alert("Tenés bajo peso");
+    }
+    
+    else if (imc > 18, imc <= 25){
+        alert("Tenés peso saludable");
+    }
+    
+    else (imc > 25);{
+        alert("Tenés sobrepeso");
+    }
+    
+    }
+    
+function calcular(peso, altura){
+        
+    var peso = Number(prompt("Ingresá tu peso en KG"));
+
+    var altura = Number(prompt("Ingresá tu altura en Mts (el punto se usa como coma)"));
+
+    var resultado = peso / Math.pow(Number(altura) , 2);
+        
+    imc = Math.round(resultado);
+        
+    return imc;
+}
+
+
